8570bb02dfbc6e90e579462061a1580ffe752e97
sandan79 <101040630+sandan79@users.noreply.github.com>
2022-05-03 20:32:17 +0700

    init code
    init code

---
+++ src/main/java/com/mmo/lookforcolors/LookforcolorsApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforcolors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforcolorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforcolorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmo/lookforcolors/LookforcolorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforcolors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforcolorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
